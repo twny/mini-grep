c21ecfa89ea8e242504b39c3fc86e38ba1115236
tawny <hitawnie@gmail.com>
2023-02-17 23:44:55 -0800

    Parse cli args into a vec and print them to stdout

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    // string
+    let query = &args[1];
+    // filepath
+    let filepath = &args[2];
+
+    // case insitive?
+    let third_arg = &args[3];
+
+    println!("first arg: {}", query);
+    println!("second arg: {}", filepath);
+    println!("third arg: {}", third_arg);
+    // println!("{:?}", args);
 }
