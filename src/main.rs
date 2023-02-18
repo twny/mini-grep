a40f07440e85b76dc8e026c8770828abfa760d15
tawny <hitawnie@gmail.com>
2023-02-18 00:54:38 -0800

    Break arg reading out into a function

--- src/main.rs
+++ src/main.rs
@@ -3,16 +3,24 @@ use std::fs;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
+    let config = parse_args(&args);
 
-    // string
-    let query = &args[1];
-    // filepath
-    let filepath = &args[2];
+    let contents = fs::read_to_string(&config.filepath).expect("Wanted to read a file");
 
-    let contents = fs::read_to_string(filepath).expect("Wanted to read a file");
-
-    println!("first arg: {query}");
-    println!("second arg: {filepath}");
+    println!("first arg: {}", &config.query);
+    println!("second arg: {}", &config.filepath);
     println!("poem\n{contents}");
     // println!("{:?}", args);
+}
+
+struct Config {
+    query: String,
+    filepath: String,
+}
+
+fn parse_args(args: &[String]) -> Config {
+    let query = &args[1].clone();
+    let filepath = &args[2].clone();
+
+    Config { query: query.to_string(), filepath: filepath.to_string() }
 }
